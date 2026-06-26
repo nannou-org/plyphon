603f8b5580657a521620118d41707f6af4b92d94
mitchmindtree <mitchmindtree@users.noreply.github.com>
2026-06-26 01:53:38 +0000

    deploy: de370d84f961e96af36564c0b69d2d0ad7b9b0b1

--- osc/example-osc.js
+++ osc/example-osc.js
@@ -138,7 +138,7 @@ function __wbg_get_imports() {
         },
         __wbindgen_cast_0000000000000001: function(arg0, arg1) {
             // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [], shim_idx: 2, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
-            const ret = makeMutClosure(arg0, arg1, __wasm_bindgen_func_elem_154);
+            const ret = makeMutClosure(arg0, arg1, __wasm_bindgen_func_elem_156);
             return addHeapObject(ret);
         },
         __wbindgen_cast_0000000000000002: function(arg0, arg1) {
@@ -161,8 +161,8 @@ function __wbg_get_imports() {
 }
 
 const lAudioContext = (typeof AudioContext !== 'undefined' ? AudioContext : (typeof webkitAudioContext !== 'undefined' ? webkitAudioContext : undefined));
-function __wasm_bindgen_func_elem_154(arg0, arg1) {
-    wasm.__wasm_bindgen_func_elem_154(arg0, arg1);
+function __wasm_bindgen_func_elem_156(arg0, arg1) {
+    wasm.__wasm_bindgen_func_elem_156(arg0, arg1);
 }
 
 function addHeapObject(obj) {
