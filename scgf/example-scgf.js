a32c86b59035bc25cf32bf37a35d8ab8dd50403b
mitchmindtree <mitchmindtree@users.noreply.github.com>
2026-06-29 06:48:13 +0000

    deploy: 711c955b4e6a27b06cc5f758adb8b84952ecc33d

--- scgf/example-scgf.js
+++ scgf/example-scgf.js
@@ -427,7 +427,7 @@ function __wbg_get_imports(memory) {
                     const a = state0.a;
                     state0.a = 0;
                     try {
-                        return __wasm_bindgen_func_elem_609(a, state0.b, arg0, arg1);
+                        return __wasm_bindgen_func_elem_610(a, state0.b, arg0, arg1);
                     } finally {
                         state0.a = a;
                     }
@@ -570,12 +570,12 @@ function __wbg_get_imports(memory) {
         },
         __wbindgen_cast_0000000000000001: function(arg0, arg1) {
             // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 1, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
-            const ret = makeMutClosure(arg0, arg1, __wasm_bindgen_func_elem_136);
+            const ret = makeMutClosure(arg0, arg1, __wasm_bindgen_func_elem_137);
             return addHeapObject(ret);
         },
         __wbindgen_cast_0000000000000002: function(arg0, arg1) {
             // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 44, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
-            const ret = makeMutClosure(arg0, arg1, __wasm_bindgen_func_elem_536);
+            const ret = makeMutClosure(arg0, arg1, __wasm_bindgen_func_elem_537);
             return addHeapObject(ret);
         },
         __wbindgen_cast_0000000000000003: function(arg0) {
@@ -618,14 +618,14 @@ function __wbg_get_imports(memory) {
 }
 
 const lAudioContext = (typeof AudioContext !== 'undefined' ? AudioContext : (typeof webkitAudioContext !== 'undefined' ? webkitAudioContext : undefined));
-function __wasm_bindgen_func_elem_136(arg0, arg1, arg2) {
-    wasm.__wasm_bindgen_func_elem_136(arg0, arg1, addHeapObject(arg2));
+function __wasm_bindgen_func_elem_137(arg0, arg1, arg2) {
+    wasm.__wasm_bindgen_func_elem_137(arg0, arg1, addHeapObject(arg2));
 }
 
-function __wasm_bindgen_func_elem_536(arg0, arg1, arg2) {
+function __wasm_bindgen_func_elem_537(arg0, arg1, arg2) {
     try {
         const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
-        wasm.__wasm_bindgen_func_elem_536(retptr, arg0, arg1, addHeapObject(arg2));
+        wasm.__wasm_bindgen_func_elem_537(retptr, arg0, arg1, addHeapObject(arg2));
         var r0 = getDataViewMemory0().getInt32(retptr + 4 * 0, true);
         var r1 = getDataViewMemory0().getInt32(retptr + 4 * 1, true);
         if (r1) {
@@ -636,8 +636,8 @@ function __wasm_bindgen_func_elem_536(arg0, arg1, arg2) {
     }
 }
 
-function __wasm_bindgen_func_elem_609(arg0, arg1, arg2, arg3) {
-    wasm.__wasm_bindgen_func_elem_609(arg0, arg1, addHeapObject(arg2), addHeapObject(arg3));
+function __wasm_bindgen_func_elem_610(arg0, arg1, arg2, arg3) {
+    wasm.__wasm_bindgen_func_elem_610(arg0, arg1, addHeapObject(arg2), addHeapObject(arg3));
 }
 
 const StreamConfigFinalization = (typeof FinalizationRegistry === 'undefined')
